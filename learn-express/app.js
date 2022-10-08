34f9d314e57b315935f847790b242a3b07714b4c
Eunbi-Lee <lyb6336@naver.com>
2022-10-09 06:55:23 +0900

    view engine pug->html로 변경

--- learn-express/app.js
+++ learn-express/app.js
@@ -1,65 +1,51 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const nunjucks = require("nunjucks");
 
-//콘솔에 나오는 GET / 200 51.267 ms 같은 로그는 모두 morgan 미들웨어에서 나온다.
-var logger = require("morgan");
-var session = require("express-session");
-var flash = require("connect-flash");
+const { sequelize } = require("./models");
+const indexRouter = require("./routes");
+const usersRouter = require("./routes/users");
+const commentsRouter = require("./routes/comments");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
-var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
-app.use(function (req, res, next) {
-  console.log(req.url, "저도 미들웨어입니다.");
-  next();
+const app = express();
+app.set("port", process.env.PORT || 3001);
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
 });
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("데이터베이스 연결 성공");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
-app.use(logger("dev"));
+app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("secret code"));
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: "secret code",
-    cookie: {
-      httpOnly: true,
-      secure: false,
-    },
-  })
-);
-app.use(flash());
-
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/comments", commentsRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
   res.status(err.status || 500);
   res.render("error");
 });
 
-module.exports = app;
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기 중");
+});
