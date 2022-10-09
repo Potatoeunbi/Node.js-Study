2a3b34194f2ea65914a5d9f9fd5b5accb82afb98
Potatoeunbi <lyb6336@naver.com>
2022-10-09 23:24:41 +0900

    node.js 9-1단원 공부 끝

--- learn-express/nodebird/app.js
+++ learn-express/nodebird/app.js
@@ -3,22 +3,25 @@ const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const path = require("path");
 const session = require("express-session");
-const flash = require("connect-flash");
-require("dotenv").config();
+const nunjucks = require("nunjucks");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const pageRouter = require("./routes/page");
 
 const app = express();
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", pug);
 app.set("port", process.env.PORT || 8001);
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
 
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("process.env.COOKIE_SECRET"));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
   session({
     resave: false,
@@ -31,21 +34,21 @@ app.use(
   })
 );
 
-app.use(flash());
 app.use("/", pageRouter);
+
 app.use((req, res, next) => {
-  const err = new Error("Not Found");
-  err.status = 404;
-  next(err);
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 });
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
   res.status(err.status || 500);
   res.render("error");
 });
 
 app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 포트에서 대기 중");
+  console.log(app.get("port"), "번 포트에서 대기중");
 });
